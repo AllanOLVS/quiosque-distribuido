70102ef6c2b32bf5157636afb141f67c34804c1e
Allan Oliveira <allan.oliveiraa009@gmail.com>
2025-12-18 11:44:58 -0300

    iniciando git no repo

---
+++ src/main/java/com/ifba_sistemasDistribuidos/quiosque_distribuido/QuiosqueDistribuidoApplication.java
@@ -0,0 +1,13 @@
+package com.ifba_sistemasDistribuidos.quiosque_distribuido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuiosqueDistribuidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuiosqueDistribuidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifba_sistemasDistribuidos/quiosque_distribuido/QuiosqueDistribuidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifba_sistemasDistribuidos.quiosque_distribuido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuiosqueDistribuidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
